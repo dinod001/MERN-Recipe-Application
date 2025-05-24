ed2f060a3d947838b94977c4db5bdf8133f838f2
DSYS <dinodimanjith404@gmail.com>
2025-05-24 06:53:20 +0530

    All schema are created

---
+++ backend/routes/recipie.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    ingredients: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    instructions: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    photo_url: {
+      type: String,
+      required: true,
+    },
+    cooking_time: {
+      type: Number,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const recipie = mongoose.model("Recipie", recipeSchema);
+export default recipie;
---
+++ backend/routes/user.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified(this.password)) return next();
+  const salt = await bcrypt.salt(10);
+  this.password = bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredpassword) {
+  return await bcrypt.compare(enteredpassword, this.password);
+};
+
+const user = mongoose.model("User", userSchema);
+export default user;
